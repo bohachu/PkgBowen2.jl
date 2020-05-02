89e416bd63261be3d187a6836bfb7ed9ddabb6c5
bohachu <cbh@cameo.tw>
2020-05-02 15:38:39 +0800

    Files generated by PkgTemplates

---
+++ src/PkgBowen2.jl
@@ -0,0 +1,5 @@
+module PkgBowen2
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using PkgBowen2
+using Test
+
+@testset "PkgBowen2.jl" begin
+    # Write your own tests here.
+end
